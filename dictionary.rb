5b731bde4bab8eef78cf4113f4934e474bcad533
“SIeep” <Brandon_n_evans@live.com>
2019-02-21 21:26:54 -0600

    Dictionary WIP

---
+++ dictionary.rb
@@ -0,0 +1,32 @@
+# get API data
+# sort through API data
+# ask user to enter a word
+# create print statement that shows definition of user input
+require 'http'
+
+loop do 
+  p "Type a word for the definition:"
+  user_word = gets.chomp
+  if user_word == "q"
+    break
+  else
+
+  end
+  puts "-----------------------------------------------------------------------------------------------------"
+
+  response_definition = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7")
+
+  response_pronounciation = HTTP.get("https://api.wordnik.com/v4/word.json/#{user_word}/pronunciations?useCanonical=false&limit=50&api_key=48dd829661f515d5abc0d03197a00582e888cc7da2484d5c7")
+
+
+  word_definition = response_definition.parse
+  word_pronounciation = response_pronounciation.parse
+
+  puts "The definitiion of '#{user_word}' is:"
+  p word_definition[0]["text"]
+
+  puts "-----------------------------------------------------------------------------------------------------"
+
+  puts "The pronounciation for '#{user_word}' is:"
+  p word_pronounciation[0]["raw"]
+end
